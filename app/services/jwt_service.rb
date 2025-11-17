be3ae91d40535a2894dec7d567502d1791ef3212
Rich Dabrowski <engineering@richd.net>
2025-11-17 15:09:26 -0800

    app/services/jwt_service.rb: add JWT_SECRET_KEY var to enable Railway deployment

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -1,5 +1,5 @@
 class JwtService
-  SECRET_KEY = Rails.application.credentials.secret_key_base
+  SECRET_KEY = ENV.fetch("JWT_SECRET_KEY") { Rails.application.credentials.secret_key_base }
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
